0e1d1320261d11c6bd24e6a5854b63d1415099cc
Daniil Laureanti <daniil.laureanti@gmail.com>
2022-03-18 16:53:01 +0200

    Layer Data Implementation

--- app/src/main/java/com/demo/composition/domain/entity/Level.kt
+++ app/src/main/java/com/demo/composition/domain/entity/Level.kt
@@ -1,8 +1,5 @@
 package com.demo.composition.domain.entity
 
-data class Level(
-    val winner: Boolean,
-    val countOfRightAnswers: Int,
-    val countOfQuestions: Int,
-    val gameSettings: GameSettings
-)
+enum class Level() {
+    TEST, EASY, NORMAL, HARD
+}
