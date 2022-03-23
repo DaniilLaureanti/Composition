f03ec8cad3f12702d390864dc501bf001e0f2a80
Daniil Laureanti <daniil.laureanti@gmail.com>
2022-03-23 14:24:34 +0200

    Implementing Parameter Values in GameFinishedFragment Using Data Binding

---
+++ app/src/main/java/com/demo/composition/presentation/BindingAdapters.kt
@@ -0,0 +1,60 @@
+package com.demo.composition.presentation
+
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.demo.composition.R
+import com.demo.composition.domain.entity.GameResult
+
+@BindingAdapter("requiredAnswers")
+fun bindRequiredAnswers(textView: TextView, count: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_score),
+        count
+    )
+}
+
+@BindingAdapter("scoreAnswers")
+fun bindScoreAnswers(textView: TextView, score: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_answers),
+        score
+    )
+}
+
+@BindingAdapter("requiredPercentage")
+fun bindRequiredPercentage(textView: TextView, percentage: Int) {
+    textView.text = String.format(
+        textView.context.getString(R.string.required_percentage),
+        percentage
+    )
+}
+
+@BindingAdapter("scorePercentage")
+fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
+    textView.text = String.format(
+        textView.context.getString(R.string.score_percentage),
+        getPercentOfRightAnswers(gameResult)
+    )
+}
+
+@BindingAdapter("resultEmoji")
+fun bindResultEmoji(imageView: ImageView, winner: Boolean) {
+    imageView.setImageResource(getSmileResId(winner))
+}
+
+private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
+    if (countOfQuestions == 0) {
+        0
+    } else {
+        ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+}
+
+private fun getSmileResId(winner: Boolean): Int {
+    return if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
+    }
+}
--- app/src/main/java/com/demo/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/demo/composition/presentation/GameFinishedFragment.kt
@@ -37,41 +37,7 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswer.text = String.format(
-                getString(R.string.required_score),
-                args.gameResult.gameSettings.minCountOfRightAnswer
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
-    }
-
-    private fun getPercentOfRightAnswers() = with(args.gameResult) {
-        if (countOfQuestions == 0) {
-            0
-        } else {
-            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
-        }
-    }
-
-    private fun getSmileResId(): Int {
-        return if (args.gameResult.winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
+        binding.gameResult = args.gameResult
     }
 
     private fun setUpClickListeners() {
